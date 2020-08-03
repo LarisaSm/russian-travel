8c8f886026e15c22672d1e71c62f1da3f476025e
Larisa Smirnova <smirnova.larisa2020@yandex.ru>
2020-08-03 16:53:43 +0300

    fix popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
 let popup = document.querySelector('.popup');
 let menuAboutButton = document.querySelector('.header__about');
 let image = popup.querySelector('.image');
+let page = document.querySelector('.page');
+let menu = document.querySelector('.header__about');
 
 let closePopupButton = popup.querySelector('.popup__close');
 let imageCloseButton = popup.querySelector('.image__close');
@@ -10,6 +12,11 @@ let imageOpenButton = popup.querySelector('.popup__link');
 function popupOpenClose (evt) {
   evt.preventDefault();
   popup.classList.toggle('popup_opened');
+  page.classList.toggle('page__stopscrolling');
+  menuAboutButton.classList.toggle('header__about_type_active');
+  imageOpenButton.classList.remove('popup__link_hide');
+  image.classList.remove('image_opened');
+  
 }
 
 function imageOpenClose (evt) {
